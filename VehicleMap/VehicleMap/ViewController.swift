d6abe5f88901e1fc0fe502c30405807d2f51a93f
Mert Koraltan <mertkoraltan@gmail.com>
2022-06-26 19:35:34 +0300

    feet(Project): create new xcode project

---
+++ VehicleMap/VehicleMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VehicleMap
+//
+//  Created by Mert Koraltan on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
